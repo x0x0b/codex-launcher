209f16dbf48de011a0f504cca90c24233bdb7f53
x0x0b <42596409+x0x0b@users.noreply.github.com>
2025-08-23 13:35:52 +0900

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,15 @@ intellijPlatform {
             Initial version
         """.trimIndent()
     }
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+    }
+
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
 }
 
 tasks {
